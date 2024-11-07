0c39cec0ad3185884cb03211351b231290998b3a
T3rex <31337aditya@gmail.com>
2024-11-08 04:00:54 +0530

    Setup basic http server and routes

---
+++ spaceX/apps/http/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { router } from "./routes/v1";
+const app = express();
+const port = 3000;
+
+app.use("/api/v1", router);
+
+app.listen(port, () => {
+  console.log(`HTTP server running on port ${port}`);
+});
