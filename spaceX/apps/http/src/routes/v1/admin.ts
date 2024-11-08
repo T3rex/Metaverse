d76cde15cd1df68590f13031180975de72adee20
T3rex <31337aditya@gmail.com>
2024-11-09 03:35:27 +0530

    Added router skeleton

---
+++ spaceX/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.post("/element/:spaceId", (req, res) => {});
+adminRouter.put("/element/:elementId", (req, res) => {});
+adminRouter.get("/avatar", (req, res) => {});
+adminRouter.get("/map", (req, res) => {});
--- spaceX/apps/http/src/routes/v1/index.ts
+++ spaceX/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { userRouter } from "./users";
+import { adminRouter } from "./admin";
+import { spaceRouter } from "./space";
 
 export const router = Router();
 
@@ -9,3 +12,11 @@ router.post("/signup", (req, res) => {
 router.post("/signin", (req, res) => {
   res.json({ message: "Signin" });
 });
+
+router.get("/elements", (req, res) => {});
+
+router.get("/avatars", (req, res) => {});
+
+router.use("/admin", adminRouter);
+router.use("/user", userRouter);
+router.use("/user", spaceRouter);
